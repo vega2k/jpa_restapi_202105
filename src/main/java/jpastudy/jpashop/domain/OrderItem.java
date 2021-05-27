38ca93e3347d9b1c78b65eaf05e7179a092c223c
vega2k <vega2k@naver.com>
2021-05-27 17:21:04 +0900

    비지니스 로직메서드 추가

--- src/main/java/jpastudy/jpashop/domain/OrderItem.java
+++ src/main/java/jpastudy/jpashop/domain/OrderItem.java
@@ -27,4 +27,22 @@ public class OrderItem {
 
     private int count; //주문 수량
 
+    //==비지니스 로직 : OrderItem 생성 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+    //==비즈니스 로직 : 주문 취소 되면 Item의 재고수량 감소 ==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+    //==비즈니스 로직 : 주문상품 전체 가격 조회 ==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
 }
