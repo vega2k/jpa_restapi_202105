92891a6e200c0360180bcfd92ba8cdcaa186a04c
vega2k <vega2k@naver.com>
2021-05-27 15:54:27 +0900

    Category 추가

--- src/main/java/jpastudy/jpashop/domain/item/Item.java
+++ src/main/java/jpastudy/jpashop/domain/item/Item.java
@@ -1,9 +1,12 @@
 package jpastudy.jpashop.domain.item;
 
+import jpastudy.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -20,5 +23,6 @@ public abstract  class Item {
 
     private int stockQuantity;
 
-
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
