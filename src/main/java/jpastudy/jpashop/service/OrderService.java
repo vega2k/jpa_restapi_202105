7de2edf9fad7e5c4e36a041a56403229bb896b3c
vega2k <vega2k@naver.com>
2021-05-28 09:39:00 +0900

    orderservice

--- src/main/java/jpastudy/jpashop/service/OrderService.java
+++ src/main/java/jpastudy/jpashop/service/OrderService.java
@@ -20,22 +20,22 @@ public class OrderService {
     private final ItemRepository itemRepository;
 
     /**
-     * 주문
+     * 주문 생성
      */
     @Transactional
     public Long order(Long memberId, Long itemId, int count) {
-    //엔티티 조회
+        //엔티티 조회
         Member member = memberRepository.findOne(memberId);
         Item item = itemRepository.findOne(itemId);
-    //배송정보 생성
+        //배송정보 생성
         Delivery delivery = new Delivery();
         delivery.setAddress(member.getAddress());
         delivery.setStatus(DeliveryStatus.READY);
-    //주문상품 생성
+        //주문상품 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
-    //주문 생성
+        //주문 생성
         Order order = Order.createOrder(member, delivery, orderItem);
-    //주문 저장
+        //주문 저장
         orderRepository.save(order);
         return order.getId();
     }
@@ -45,9 +45,9 @@ public class OrderService {
      */
     @Transactional
     public void cancelOrder(Long orderId) {
-    //주문 엔티티 조회
+        //주문 엔티티 조회
         Order order = orderRepository.findOne(orderId);
-    //주문 취소
+        //주문 취소
         order.cancel();
     }
 
