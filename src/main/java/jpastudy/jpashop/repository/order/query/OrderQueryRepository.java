72469188ddbd6efd0e1f76d1a34ff53fdf964f00
vega2k <vega2k@naver.com>
2021-05-28 16:42:01 +0900

    OrderQueryRepository추가

---
+++ src/main/java/jpastudy/jpashop/repository/order/query/OrderQueryRepository.java
@@ -0,0 +1,26 @@
+package jpastudy.jpashop.repository.order.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderQueryRepository {
+    private final EntityManager em;
+
+    /**
+     * 1:N 관계(컬렉션)를 제외한 Order, Member, Delivery를 한번에 조회
+     */
+    private List<OrderQueryDto> findOrders() {
+        return em.createQuery(
+                "select new jpastudy.jpashop.repository.order.query.OrderQueryDto"+
+                        "(o.id, m.name, o.orderDate, o.status, d.address)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d", OrderQueryDto.class)
+                .getResultList();
+    } //findOrders
+}
