706c415c70d375ad4429c56ac3023cfca9c636cd
vega2k <vega2k@naver.com>
2021-05-28 13:45:41 +0900

    controller에서 강제지연로딩

--- src/main/java/jpastudy/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpastudy/jpashop/api/OrderSimpleApiController.java
@@ -21,6 +21,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+        }
         return all;
     }
 }
