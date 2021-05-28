5e42e249063c45674bdaa28db1a5cd56cf387357
vega2k <vega2k@naver.com>
2021-05-28 17:41:17 +0900

    v4 추가

--- src/main/java/jpastudy/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpastudy/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Repository
 @RequiredArgsConstructor
@@ -49,4 +53,24 @@ public class OrderQueryRepository {
         });
         return orders;
     }
+
+    //OrderId 목록
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream()
+                .map(o -> o.getOrderId())
+                .collect(toList());
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "select new jpastudy.jpashop.repository.order.query.OrderItemQueryDto("+
+                        "oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        " from OrderItem oi" +
+                        " join oi.item i" +
+                        " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+        return orderItems.stream().collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
 }
--- src/test/java/jpastudy/jpashop/GroupingTest.java
+++ src/test/java/jpastudy/jpashop/GroupingTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GroupingTest {
     @Test
@@ -11,15 +13,18 @@ public class GroupingTest {
         List<Transaction> transactions =
                 Arrays.asList( new Transaction(Currency.EUR, 1500.0),
                         new Transaction(Currency.USD, 2300.0),
-                        new Transaction(Currency.GBP, 9900.0),
+                        new Transaction(Currency.USD, 9900.0),
                         new Transaction(Currency.EUR, 1100.0),
                         new Transaction(Currency.JPY, 7800.0),
                         new Transaction(Currency.CHF, 6700.0));
 
         //Map<Currency, List<Transaction>>
+        Map<Currency, List<Transaction>> map1 = transactions.stream() //Stream<Transaction>
+                .collect(Collectors.groupingBy(Transaction::getCurrency));
+                //.collect(Collectors.groupingBy(tx -> tx.getCurrency()));
+        System.out.println(map1);
 
         //Map<Currency, Map<Boolean, List<Transaction>>>
-
 
     }
 
@@ -42,7 +47,7 @@ public class GroupingTest {
 
         @Override
         public String toString() {
-            return currency + " " + value;
+            return currency + " : " + value;
         }
     }
 
